d6cf15802c6a2f7f9937e1583853835da31210a7
Amit Kumar Sutradhar <amit596450@gmail.com>
2022-10-11 13:53:31 +0600

    Fix netlify Page not found issue and add basic components

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Statistics = () => {
+    return (
+        <div>
+            <h2>Hello from blog js.</h2>
+        </div>
+    );
+};
+
+export default Statistics;
---
+++ src/components/Topics/Topics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Topics = () => {
+    return (
+        <div>
+            <h2>Hello form topic js</h2>
+        </div>
+    );
+};
+
+export default Topics;

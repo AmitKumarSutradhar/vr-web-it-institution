a13e11d54544e3d30afefa91feda21adc2cbff96
Amit Kumar Sutradhar <amit596450@gmail.com>
2022-10-11 22:42:17 +0600

    Statistics Chart added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Statistics = () => {
+    const data = useLoaderData();
+    // console.log(data.data);
     return (
         <div>
-            <h2>Hello from blog js.</h2>
+            <ResponsiveContainer width="60%" height={400} className='mx-auto'>
+                <LineChart width={700} data={data.data}>
+                    <Line type="monotone" dataKey="total" stroke="#8884d8"></Line>
+                    <CartesianGrid></CartesianGrid>
+                    <XAxis dataKey="name"></XAxis>
+                    <YAxis></YAxis>
+                    <Tooltip></Tooltip>
+                </LineChart>
+            </ResponsiveContainer>
         </div>
     );
 };

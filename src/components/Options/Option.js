70ea179f2250ce0776e39faf140a6e7533403185
Amit Kumar Sutradhar <amit596450@gmail.com>
2022-10-12 13:27:12 +0600

    Toast Added for verify ans

--- src/components/Options/Option.js
+++ src/components/Options/Option.js
@@ -1,24 +1,30 @@
 import React from 'react';
-// import { ToastContainer, toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './Option.css';
 
 const Option = ({ option, index, question }) => {
-    // console.log(question.id);
-    // const notify = () => {
-    //     toast("Wow so easy!");
-    // }
+    // console.log(question.correctAnswer);
+    const { correctAnswer } = question;
+    const notify = ({ option, correctAnswer }) => {
+        if (option === correctAnswer) {
+            toast('Your answer is correct');
+        } else {
+            toast('Your answer is wrong.');
+        }
+        // console.log(option, correctAnswer);
+    }
 
     return (
         <div className="">
             <div className='text-left my-2 flex content-center'>
-                <input type="radio" id={question.id} name={question.id} value={option} />{option}
 
-                {/* <button onClick={() => notify()}>
-                    
-                </button> */}
+                <div className="" onClick={() => notify({ option, correctAnswer })}>
+                    <li>{option}</li>
+                </div>
+
             </div>
-            {/* <ToastContainer></ToastContainer> */}
+            <ToastContainer></ToastContainer>
         </div>
 
     );
